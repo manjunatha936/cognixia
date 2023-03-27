ebfa8965b80d577e81e3530ad9941f841d28d3d8
manjunatha <manjunath@lollypop.design>
2023-03-27 10:05:10 +0530

    updated with all the pages

---
+++ src/js/DMC.js
@@ -0,0 +1,65 @@
+const swiper = new Swiper(".DigitalSlider", {
+  grabCursor: true,
+  //   slidesPerView: 3.7,
+  spaceBetween: 15,
+  speed: 1000,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // If we need pagination
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    500: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2.3,
+    },
+    1024: {
+      slidesPerView: 2.8,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3.2,
+    },
+  },
+});
+
+const TransformationSlider = new Swiper(".TransformationSlider", {
+  grabCursor: true,
+  //   slidesPerView: 3.7,
+  spaceBetween: 15,
+  speed: 1000,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // If we need pagination
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    500: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2.3,
+    },
+    1024: {
+      slidesPerView: 2.8,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3.2,
+    },
+  },
+});
