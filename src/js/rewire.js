bac8c3afdf08505456e0edd6f29013c1e5d5162d
AdithyaB011 <adithya.besta@terralogic.com>
2023-05-31 14:17:19 +0530

    rewire form and search

--- src/js/rewire.js
+++ src/js/rewire.js
@@ -30,3 +30,74 @@ const swiper = new Swiper(".Myswiper-rewire", {
     },
   },
 });
+
+// custom_dropdown
+$(document).ready(function() {
+  $('.dropdown').each(function() {
+    var dropdownWrapper = $(this);
+    var dropdownBtn = dropdownWrapper.find('.dropdown__button');
+    var dropdownList = dropdownWrapper.find('.dropdown__list');
+    var dropdownItems = dropdownList.find('.dropdown__list-item');
+    var dropdownInput = dropdownWrapper.find('.dropdown__input');
+
+    dropdownBtn.click(function() {
+      dropdownList.toggleClass('dropdown__list_visible');
+      $(this).toggleClass('dropdown__button_active');
+    });
+
+    dropdownItems.click(function(e) {
+      dropdownItems.removeClass('dropdown__list-item_active');
+      $(this).addClass('dropdown__list-item_active');
+      dropdownBtn.text($(this).text());
+      dropdownInput.val($(this).data('value'));
+      dropdownList.removeClass('dropdown__list_visible');
+    });
+
+    $(document).click(function(e) {
+      if (!$(e.target).is(dropdownBtn)) {
+        dropdownBtn.removeClass('dropdown__button_active');
+        dropdownList.removeClass('dropdown__list_visible');
+      }
+    });
+
+    $(document).keydown(function(e) {
+      if (e.key === 'Tab' || e.key === 'Escape') {
+        dropdownBtn.removeClass('dropdown__button_active');
+        dropdownList.removeClass('dropdown__list_visible');
+      }
+    });
+  });
+});
+  // form-success
+$('#contact-form').parsley().on('field:validated', function() {
+  var ok = $('.parsley-error').length === 0;
+  
+  console.log("validated")
+  $('#error').show();  
+ })
+ .on('form:submit', function() {
+     $("#contact-success").show()
+     $(".contact-row").hide()
+     return false; 
+ })
+// search
+
+var content = [
+  { title: 'Microsoft' },
+  { title: 'java' },
+  { title: 'javascript' },
+  { title: 'Machine Learning & Artificial Intelligence' },
+  { title: 'Robotic Process Automation' },
+  { title: 'Business Trainings ' },
+  { title: 'Application Development & Design' },
+  { title: 'Cyber Security' },
+  { title: 'Internet of Things' },
+  { title: 'Robotic Process Automation' },
+  { title: 'Cloud Computing' },
+  // etc
+];
+$('.ui.search')
+.search({
+  source: content
+})
+;
